7b83f45bc06a8fe1fee77ce16edb1157fae54832
cedric-neighborschools <cedric@neighborschools.com>
2019-12-14 17:06:39 -0500

    release

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ea6b69d237849247e652";
+/******/ 	var hotCurrentHash = "06c3769587a51c86ca79";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
